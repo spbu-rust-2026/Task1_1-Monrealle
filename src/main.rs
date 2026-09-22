7818ccf6b81c30661d91e4f889374da48bb30671
Stanislav Klimovich <stasklimovish@gmail.com>
2026-09-23 02:12:09 +0300

    Task 1 Complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+    
+    let mut numbers = input.split_whitespace();
+
+    let a: i32 = numbers.next().unwrap().parse().unwrap();
+    let b: i32 = numbers.next().unwrap().parse().unwrap();
+
+    println!("{}", a + b);
 }
