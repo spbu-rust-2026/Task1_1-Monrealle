a02d64f2ac5603c49f7798daf60e6d47a83ea34d
Stanislav Klimovich <stasklimovish@gmail.com>
2026-09-23 02:17:11 +0300

    Formatting fix

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).unwrap();
-    
+
     let mut numbers = input.split_whitespace();
 
     let a: i32 = numbers.next().unwrap().parse().unwrap();
